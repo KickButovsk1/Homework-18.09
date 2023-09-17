f541237ec905411df7d659e58c62b2423a8854f7
KickButovsk1 <144268614+KickButovsk1@users.noreply.github.com>
2023-09-17 20:18:20 +0500

    Add files via upload

---
+++ HomeWork 18.09 2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_18._09_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Первое число: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Второе число: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Первое число: " + B);
+            Console.WriteLine("Второе число: " + A);
+            Console.ReadKey();
+
+        }
+    }
+}
